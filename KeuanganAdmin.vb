29d0045ca8d2c2b053d2682d9a0c074c098179b8
Habibal28 <emzob30@gmail.com>
2022-07-08 12:42:57 +0700

    data

---
+++ KeuanganAdmin.vb
@@ -0,0 +1,40 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.DateTime
+Public Class KeuanganAdmin
+    Dim npm
+    Sub tampildata()
+        DS = New DataSet
+        STR = "select * from record_pembayaran"
+        DA = New MySqlDataAdapter(STR, con)
+        DA.Fill(DS)
+
+        DataGridView1.DataSource = DS.Tables(0)
+    End Sub
+
+    Private Sub KeuanganAdmin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        sambung()
+        tampildata()
+    End Sub
+
+    Private Sub input_keuangan_Click(sender As Object, e As EventArgs) Handles input_keuangan.Click
+        CMD = New MySqlCommand
+        CMD.Connection = con
+        STR = "insert into record_pembayaran values ('""','" & npm_txt.Text & "','" & jenis_txt.Text & "','" & tgl_txt.Text & "','" & nominal_txt.Text & "')"
+        CMD.CommandText = STR
+        CMD.ExecuteNonQuery()
+        MessageBox.Show("Data Berhasil Ditambahkan", "Notice")
+        tampildata()
+    End Sub
+
+    Private Sub tgl_txt_ValueChanged(sender As Object, e As EventArgs) Handles tgl_txt.ValueChanged
+        tgl_txt.CustomFormat = "dd/MM/yyyy"
+    End Sub
+
+    Private Sub nama_txt_TextChanged(sender As Object, e As EventArgs) Handles nama_txt.TextChanged
+
+    End Sub
+
+    Private Sub npm_txt_TextChanged(sender As Object, e As EventArgs) Handles npm_txt.TextChanged
+
+    End Sub
+End Class
---
+++ matakuliah.Designer.vb
@@ -0,0 +1,368 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class matakuliah
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnbatal = New System.Windows.Forms.Button()
+        Me.btnhapus = New System.Windows.Forms.Button()
+        Me.btnedit = New System.Windows.Forms.Button()
+        Me.btnsimpan = New System.Windows.Forms.Button()
+        Me.dgv = New System.Windows.Forms.DataGridView()
+        Me.txt_kode = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txt_sks = New System.Windows.Forms.TextBox()
+        Me.txt_namamatakuliah = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txt_semester = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txt_namakelas = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnbatal
+        '
+        Me.btnbatal.Location = New System.Drawing.Point(837, 381)
+        Me.btnbatal.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnbatal.Name = "btnbatal"
+        Me.btnbatal.Size = New System.Drawing.Size(86, 31)
+        Me.btnbatal.TabIndex = 22
+        Me.btnbatal.Text = "BATAL"
+        Me.btnbatal.UseVisualStyleBackColor = True
+        '
+        'btnhapus
+        '
+        Me.btnhapus.Location = New System.Drawing.Point(711, 381)
+        Me.btnhapus.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnhapus.Name = "btnhapus"
+        Me.btnhapus.Size = New System.Drawing.Size(86, 31)
+        Me.btnhapus.TabIndex = 21
+        Me.btnhapus.Text = "HAPUS"
+        Me.btnhapus.UseVisualStyleBackColor = True
+        '
+        'btnedit
+        '
+        Me.btnedit.Location = New System.Drawing.Point(589, 381)
+        Me.btnedit.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnedit.Name = "btnedit"
+        Me.btnedit.Size = New System.Drawing.Size(86, 31)
+        Me.btnedit.TabIndex = 20
+        Me.btnedit.Text = "EDIT"
+        Me.btnedit.UseVisualStyleBackColor = True
+        '
+        'btnsimpan
+        '
+        Me.btnsimpan.Location = New System.Drawing.Point(477, 381)
+        Me.btnsimpan.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnsimpan.Name = "btnsimpan"
+        Me.btnsimpan.Size = New System.Drawing.Size(86, 31)
+        Me.btnsimpan.TabIndex = 19
+        Me.btnsimpan.Text = "SIMPAN"
+        Me.btnsimpan.UseVisualStyleBackColor = True
+        '
+        'dgv
+        '
+        Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv.Location = New System.Drawing.Point(338, 433)
+        Me.dgv.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.dgv.Name = "dgv"
+        Me.dgv.RowHeadersWidth = 51
+        Me.dgv.RowTemplate.Height = 25
+        Me.dgv.Size = New System.Drawing.Size(793, 200)
+        Me.dgv.TabIndex = 14
+        '
+        'txt_kode
+        '
+        Me.txt_kode.Location = New System.Drawing.Point(537, 197)
+        Me.txt_kode.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txt_kode.Name = "txt_kode"
+        Me.txt_kode.Size = New System.Drawing.Size(114, 27)
+        Me.txt_kode.TabIndex = 13
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(375, 204)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 20)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "KODE"
+        '
+        'txt_sks
+        '
+        Me.txt_sks.Location = New System.Drawing.Point(922, 237)
+        Me.txt_sks.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txt_sks.Name = "txt_sks"
+        Me.txt_sks.Size = New System.Drawing.Size(114, 27)
+        Me.txt_sks.TabIndex = 26
+        '
+        'txt_namamatakuliah
+        '
+        Me.txt_namamatakuliah.Location = New System.Drawing.Point(922, 189)
+        Me.txt_namamatakuliah.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txt_namamatakuliah.Name = "txt_namamatakuliah"
+        Me.txt_namamatakuliah.Size = New System.Drawing.Size(114, 27)
+        Me.txt_namamatakuliah.TabIndex = 25
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(760, 245)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(34, 20)
+        Me.Label4.TabIndex = 24
+        Me.Label4.Text = "SKS"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(760, 197)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(147, 20)
+        Me.Label5.TabIndex = 23
+        Me.Label5.Text = "NAMA MATAKULIAH"
+        '
+        'txt_semester
+        '
+        Me.txt_semester.Location = New System.Drawing.Point(922, 291)
+        Me.txt_semester.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txt_semester.Name = "txt_semester"
+        Me.txt_semester.Size = New System.Drawing.Size(114, 27)
+        Me.txt_semester.TabIndex = 28
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(760, 301)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(79, 20)
+        Me.Label6.TabIndex = 27
+        Me.Label6.Text = "SEMESTER"
+        '
+        'txt_namakelas
+        '
+        Me.txt_namakelas.FormattingEnabled = True
+        Me.txt_namakelas.Location = New System.Drawing.Point(537, 249)
+        Me.txt_namakelas.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txt_namakelas.Name = "txt_namakelas"
+        Me.txt_namakelas.Size = New System.Drawing.Size(138, 28)
+        Me.txt_namakelas.TabIndex = 29
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(375, 257)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(99, 20)
+        Me.Label3.TabIndex = 30
+        Me.Label3.Text = "NAMA KELAS"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Panel1.Controls.Add(Me.Label10)
+        Me.Panel1.Location = New System.Drawing.Point(256, 0)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(946, 127)
+        Me.Panel1.TabIndex = 33
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label10.ForeColor = System.Drawing.Color.White
+        Me.Label10.Location = New System.Drawing.Point(53, 44)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(172, 41)
+        Me.Label10.TabIndex = 14
+        Me.Label10.Text = "Mahasiswa"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.White
+        Me.Panel2.Controls.Add(Me.Button3)
+        Me.Panel2.Controls.Add(Me.Button4)
+        Me.Panel2.Controls.Add(Me.Button2)
+        Me.Panel2.Controls.Add(Me.Button5)
+        Me.Panel2.Controls.Add(Me.PictureBox1)
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Location = New System.Drawing.Point(2, 3)
+        Me.Panel2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(254, 715)
+        Me.Panel2.TabIndex = 32
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button3.ForeColor = System.Drawing.Color.White
+        Me.Button3.Location = New System.Drawing.Point(0, 321)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(253, 60)
+        Me.Button3.TabIndex = 16
+        Me.Button3.Text = "Kelas"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button4.ForeColor = System.Drawing.Color.White
+        Me.Button4.Location = New System.Drawing.Point(0, 256)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(254, 60)
+        Me.Button4.TabIndex = 17
+        Me.Button4.Text = "Mahasiswa"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(0, 387)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(254, 60)
+        Me.Button2.TabIndex = 15
+        Me.Button2.Text = "Mata Kuliah"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button5.ForeColor = System.Drawing.Color.White
+        Me.Button5.Location = New System.Drawing.Point(0, 191)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(254, 60)
+        Me.Button5.TabIndex = 18
+        Me.Button5.Text = "Dosen"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.DataMaster.My.Resources.Resources.Logo_ITATS
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(254, 128)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 14
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(0, 127)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(254, 60)
+        Me.Button1.TabIndex = 14
+        Me.Button1.Text = "Dashboard"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'matakuliah
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1201, 721)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txt_namakelas)
+        Me.Controls.Add(Me.txt_semester)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txt_sks)
+        Me.Controls.Add(Me.txt_namamatakuliah)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnbatal)
+        Me.Controls.Add(Me.btnhapus)
+        Me.Controls.Add(Me.btnedit)
+        Me.Controls.Add(Me.btnsimpan)
+        Me.Controls.Add(Me.dgv)
+        Me.Controls.Add(Me.txt_kode)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "matakuliah"
+        Me.Text = "matakuliah"
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnbatal As Button
+    Friend WithEvents btnhapus As Button
+    Friend WithEvents btnedit As Button
+    Friend WithEvents btnsimpan As Button
+    Friend WithEvents dgv As DataGridView
+    Friend WithEvents txt_kode As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txt_sks As TextBox
+    Friend WithEvents txt_namamatakuliah As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txt_semester As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txt_namakelas As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+End Class
